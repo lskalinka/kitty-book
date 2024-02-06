3e4113439d01d824b134cc5b85c00c09e34f21e0
Lina Kalinka <lina.kalinka@gmail.com>
2024-02-06 23:50:24 +0300

    переделать рандомизацию

--- scripts.js
+++ scripts.js
@@ -4,23 +4,48 @@ let texts = [
   '3'
 ];
 
+
 function getRandomElement(arr) {
-  let randIndex = Math.floor(Math.random() * arr.length);
-  console.log(randIndex);
+  const randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
 }
 
-let button = document.querySelector('.randomiser__button');
-let text = document.querySelector('.randomiser__text');
+const button = document.querySelector('.randomiser__button');
+const text = document.querySelector('.randomiser__text');
 
 button.addEventListener('click', function () {
-  let randomElement = getRandomElement(texts);
+  const randomElement = getRandomElement(texts);
   text.textContent = randomElement;
 });
 
+fetch('texts.txt')
+.then(response => response.text())
+.then(data => {
+  texts = data.split(/\n\n\n/);
+});
 
-$.getJSON(
-  "https://docs.google.com/document/d/e/2PACX-1vTj_xPZTVOydGcE0ZjRG7YBaluUH11cWmgXEXxuE5P5q41YoQBnFdj-chY_ckyIqsekvQR11nH7Oq75/pub",
-  function(data){
-  console.log(data);
-})
+
+
+
+// Подключение к Google Drive API
+gapi.client.load('drive', 'v3', () => {
+  // Запрос на загрузку текста из Google-документа
+  gapi.client.drive.files.export({
+    fileId: '1Mx7S3CttoFEIULEPtBBAvXcWcJDgweQiAdqQfPC1x-E',
+    mimeType: 'text/plain'
+  })
+  .then(response => {
+    var text = response.body;
+    // Создание локального текстового файла
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', 'texts.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  })
+  .catch(err => {
+    console.error('Error loading text from Google Doc', err);
+  });
+});
